49d34675fbabb1c70abaf27ed419ab623dd8b6d1
Richard Layton <rlayton@gmail.com>
2020-10-29 20:19:07 -0600

    Enable saved postions

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1755,7 +1755,7 @@
 //
 // G60/G61 Position Save and Return
 //
-//#define SAVED_POSITIONS 1         // Each saved position slot costs 12 bytes
+#define SAVED_POSITIONS 1         // Each saved position slot costs 12 bytes
 
 //
 // G2/G3 Arc Support

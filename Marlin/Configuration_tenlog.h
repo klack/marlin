279761c06157d97108ebcf3f99be0e76ffe5c98a
Richard Layton <rlayton@gmail.com>
2020-12-14 20:37:12 -0700

    Incremental

--- Marlin/Configuration_tenlog.h
+++ Marlin/Configuration_tenlog.h
@@ -1,45 +1,42 @@
-#define X_DRIVER_TYPE  A4988
-#define Y_DRIVER_TYPE  A4988
-#define Z_DRIVER_TYPE  A4988
-#define X2_DRIVER_TYPE A4988
-#define Y2_DRIVER_TYPE A4988
-#define Z2_DRIVER_TYPE A4988
-#define Z3_DRIVER_TYPE A4988
-#define Z4_DRIVER_TYPE A4988
-#define E0_DRIVER_TYPE A4988
-#define E1_DRIVER_TYPE A4988
+#if ENABLED(DriverA4988)
+  #define DriverType A4988
+  #define INVERT_X_DIR true
+  #define INVERT_Y_DIR false
+  #define INVERT_Z_DIR true
+  #define INVERT_E0_DIR false
+  #define INVERT_E1_DIR true
+#endif
 
-#define X_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
-#define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Z_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
-#define X_MAX_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
-#define Y_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Z_MAX_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
-#define Z_MIN_PROBE_ENDSTOP_INVERTING false // Set to true to invert the logic of the probe.
-#define INVERT_X_DIR true
-#define INVERT_Y_DIR false
-#define INVERT_Z_DIR true
-#define INVERT_E0_DIR false
-#define INVERT_E1_DIR true
+#if ENABLED(MachineTL_D3)
+  #define X_BED_SIZE        300
+  #define Y_BED_SIZE        300
+  #define TOOL_CHANGE_AREA  25 
+  #define X_MIN_POS -48 // Travel limits (mm) after homing, corresponding to endstop positions.
+  #define Y_MIN_POS 0
+  #define Z_MIN_POS 0
+  #define X_MAX_POS X_BED_SIZE
+  #define Y_MAX_POS Y_BED_SIZE + TOOL_CHANGE_AREA
+  #define Z_MAX_POS 350
+  #define X2_MIN_POS     5      // Set a minimum to ensure the  second X-carriage can't hit the parked first X-carriage
+  #define X2_MAX_POS   353       // Set this to the distance between toolheads when both heads are homed
+  #define X2_HOME_DIR    1       // Set to 1. The second X-carriage always homes to the maximum endstop position
+  #define DEFAULT_DUPLICATION_X_OFFSET 150  
+#endif
 
-#define X_BED_SIZE        300
-#define Y_BED_SIZE        300
-#define TOOL_CHANGE_AREA  25 
-
-#define X_MIN_POS -48 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define Y_MIN_POS 0
-#define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE + TOOL_CHANGE_AREA
-#define Z_MAX_POS 350
-
-//Configuration advanced
+// Common
 #define DUAL_X_CARRIAGE
 #define X1_MIN_POS X_MIN_POS   // Set to X_MIN_POS
 #define X1_MAX_POS X_BED_SIZE  // Set a maximum so the first X-carriage can't hit the parked second X-carriage
-#define X2_MIN_POS     5      // Set a minimum to ensure the  second X-carriage can't hit the parked first X-carriage
-#define X2_MAX_POS   353       // Set this to the distance between toolheads when both heads are homed
-#define X2_HOME_DIR    1       // Set to 1. The second X-carriage always homes to the maximum endstop position
 #define X2_HOME_POS X2_MAX_POS // Default X2 home position. Set to X2_MAX_POS.
 #define DEFAULT_DUAL_X_CARRIAGE_MODE DXC_AUTO_PARK_MODE
-#define DEFAULT_DUPLICATION_X_OFFSET 150
+
+#define X_DRIVER_TYPE  DriverType
+#define Y_DRIVER_TYPE  DriverType
+#define Z_DRIVER_TYPE  DriverType
+#define X2_DRIVER_TYPE DriverType
+#define Y2_DRIVER_TYPE DriverType
+#define Z2_DRIVER_TYPE DriverType
+#define Z3_DRIVER_TYPE DriverType
+#define Z4_DRIVER_TYPE DriverType
+#define E0_DRIVER_TYPE DriverType
+#define E1_DRIVER_TYPE DriverType

67250e4bbbca108371b15748629595183b6a9356
Andrew Seago <andrewseago@me.com>
2021-05-26 15:11:37 -0700

    Configuration_adv.h line 1617   #define BABYSTEP_MULTIPLICATOR_XY has an "a" in the number so it's 0.05a and not 0.05

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1614,7 +1614,7 @@
   #define BABYSTEP_INVERT_Z false           // Change if Z babysteps should go the other way
   //#define BABYSTEP_MILLIMETER_UNITS       // Specify BABYSTEP_MULTIPLICATOR_(XY|Z) in mm instead of micro-steps
   #define BABYSTEP_MULTIPLICATOR_Z  0.05       // (steps or mm) Steps or millimeter distance for each Z babystep
-  #define BABYSTEP_MULTIPLICATOR_XY 0.05a       // (steps or mm) Steps or millimeter distance for each XY babystep
+  #define BABYSTEP_MULTIPLICATOR_XY 0.05       // (steps or mm) Steps or millimeter distance for each XY babystep
 
   //#define DOUBLECLICK_FOR_Z_BABYSTEPPING  // Double-click on the Status Screen for Z Babystepping.
   #if ENABLED(DOUBLECLICK_FOR_Z_BABYSTEPPING)

09c5cabf877386492c28a753245a1f068567ee98
Richard Layton <rlayton@gmail.com>
2021-11-05 21:41:00 -0600

    a9 extruders

--- Marlin/Configuration_tenlog.h
+++ Marlin/Configuration_tenlog.h
@@ -460,9 +460,16 @@
 #define Z2_DRIVER_TYPE DriverType
 #define Z3_DRIVER_TYPE DriverType
 #define Z4_DRIVER_TYPE DriverType
+
 #if ENABLED(A9EXT)
   #define E0_DRIVER_TYPE A4988
   #define E1_DRIVER_TYPE A4988
+  #define LIN_ADVANCE
+  #define LIN_ADVANCE_K 0
+  #undef INVERT_E0_DIR
+	#undef INVERT_E1_DIR
+	#define INVERT_E0_DIR false
+	#define INVERT_E1_DIR true
 #else
   #define E0_DRIVER_TYPE DriverType
   #define E1_DRIVER_TYPE DriverType

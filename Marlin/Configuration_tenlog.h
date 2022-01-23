93ade96c4df63ea813634f3b54b8bc7e0d990603
klack <rlayton@gmail.com>
2022-01-23 14:58:09 -0700

    Turn on all configs

--- Marlin/Configuration_tenlog.h
+++ Marlin/Configuration_tenlog.h
@@ -415,7 +415,7 @@
 
 #define NOZZLE_PARK_POINT { (X_MIN_POS), (Y_BED_SIZE), 20 }  //??
 
-// Output bed size info
+// Output bed size in
 // #pragma message "Y_MIN_POS: " STR(Y_MIN_POS)  " Y_MAX_POS: " STR(Y_MAX_POS)  " Y_BED_SIZE: " STR(Y_BED_SIZE)
 // #pragma message "X_MIN_POS: " STR(X_MIN_POS)  " X_MAX_POS: " STR(X_MAX_POS)  " X_BED_SIZE: " STR(X_BED_SIZE)
 // #pragma message "X2_MAX_POS: " STR(X2_MAX_POS)

456a1c194bbac2b77e0d377ced935bccceb2a722
Richard Layton <rlayton@gmail.com>
2022-10-12 23:54:47 -0600

    M600 wip

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1485,7 +1485,7 @@
   // Commands to execute on filament runout.
   // With multiple runout sensors use the %c placeholder for the current tool in commands (e.g., "M600 T%c")
   // NOTE: After 'M412 H1' the host handles filament runout and this script does not apply.
-  #define FILAMENT_RUNOUT_SCRIPT "M600 T%c"
+  #define FILAMENT_RUNOUT_SCRIPT "G60 S0\nM600\nG61 S0\n"
   // After a runout is detected, continue printing this length of filament
   // before executing the runout script. Useful for a sensor at the end of
   // a feed tube. Requires 4 bytes SRAM per sensor, plus 4 bytes overhead.
